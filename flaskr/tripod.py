c7f73e935d0c0e882e521c61220b252354c18b47
Ard Scheirlynck <ardscheirlynck@Ards-MacBook-Pro-2.local>
2018-08-13 22:17:47 +0200

    intial position

--- flaskr/tripod.py
+++ flaskr/tripod.py
@@ -6,27 +6,29 @@ class Tripod(object):
         self.horizontal = 0
         self.vertical = 0
         self.step_size = 5
+        pantilthat.pan(0)
+        pantilthat.tilt(0)
 
     def left(self):
-        if (-85 < self.horizontal):
+        if (-80 < self.horizontal):
             self.horizontal = self.horizontal - self.step_size
         print('moving left ' + str(self.horizontal))
         pantilthat.pan(self.horizontal)
 
     def right(self):
-        if (self.horizontal < 85):
+        if (self.horizontal < 80):
             self.horizontal = self.horizontal + self.step_size
         print('moving right ' + str(self.horizontal))
         pantilthat.pan(self.horizontal)
 
     def up(self):
-        if (-85 < self.vertical):
+        if (-80 < self.vertical):
             self.vertical = self.vertical - self.step_size
         print('moving up ' + str(self.vertical))
         pantilthat.pan(self.vertical)
 
     def down(self):
-        if (self.vertical < 85):
+        if (self.vertical < 80):
             self.vertical = self.vertical + self.step_size
         print('moving down ' + str(self.vertical))
         pantilthat.pan(self.vertical)
